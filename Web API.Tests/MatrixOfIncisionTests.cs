0bcd3132f628c3a1faeccc570141d2d858b60227
Petro Horokh <petrohorokh@gmail.com>
2024-02-17 16:56:45 +0200

    feat: add unit tests for web API

---
+++ Web API.Tests/MatrixOfIncisionTests.cs
@@ -0,0 +1,28 @@
+﻿using Web_API.Models;
+
+namespace Web_API.Tests;
+
+[TestFixture]
+public class MatrixOfIncisionTests
+{
+    public static IEnumerable<TestCaseData> DivideCases
+    {
+        get
+        {
+            yield return new TestCaseData(3, new RequestBody { Matrix = new List<List<int>> { new List<int> { 0, 1, 0 }, new List<int> { 1, 1, 0 }, new List<int> { 0, 1, 0 } }, AllowDiagonals = true });
+            yield return new TestCaseData(2, new RequestBody { Matrix = new List<List<int>> { new List<int> { 0, 1, 0 }, new List<int> { 0, 1, 0 }, new List<int> { 0, 1, 0 } }, AllowDiagonals = false });
+            yield return new TestCaseData(1, new RequestBody { Matrix = new List<List<int>> { new List<int> { 1, 1, 0 }, new List<int> { 1, 1, 1 }, new List<int> { 1, 1, 1 } }, AllowDiagonals = true });
+            yield return new TestCaseData(0, new RequestBody { Matrix = new List<List<int>> { new List<int> { 1, 1, 1 }, new List<int> { 1, 1, 1 }, new List<int> { 1, 1, 1 } }, AllowDiagonals = true });
+        }
+    }
+
+    [TestCaseSource(nameof(DivideCases))]
+    public async Task GetNumberOfZones_ShouldReturnCorrectResult_WhenGivenMatrixAndDiagonalsPermission(int amountOfPieces, RequestBody data)
+    {
+        var matrixIncision = new MatrixOfIncision(data.Matrix, data.AllowDiagonals);
+
+        var result = await matrixIncision.GetNumberOfZones();
+
+        Assert.That(result.Equals(amountOfPieces));
+    }
+}
---
+++ Web API.Tests/SheetIncisionControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Web_API.Controllers;
+
+namespace Web_API.Tests;
+
+[TestFixture]
+public class SheetIncisionControllerTests
+{
+    private SheetIncisionController _controller = null!;
+
+    public static IEnumerable<TestCaseData> DivideCases
+    {
+        get
+        {
+            yield return new TestCaseData(3, "{\r\n  \"matrix\": [\r\n    [ 0, 1, 0 ],\r\n    [ 1, 1, 0 ],\r\n    [ 0, 1, 0 ]\r\n  ],\r\n  \"allowDiagonals\": true\r\n}");
+            yield return new TestCaseData(5, "{\r\n  \"matrix\": [\r\n    [ 0, 1, 0 ],\r\n    [ 1, 0, 1 ],\r\n    [ 0, 1, 0 ]\r\n  ],\r\n  \"allowDiagonals\": false\r\n}");
+            yield return new TestCaseData(1, "{\r\n  \"matrix\": [\r\n    [ 1, 1, 0 ],\r\n    [ 1, 0, 0 ],\r\n    [ 1, 1, 1 ]\r\n  ],\r\n  \"allowDiagonals\": true\r\n}");
+            yield return new TestCaseData(1, "{\r\n  \"matrix\": [\r\n    [ 1, 1, 1 ],\r\n    [ 0, 0, 0 ],\r\n    [ 1, 1, 1 ]\r\n  ],\r\n  \"allowDiagonals\": true\r\n}");
+        }
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _controller = new SheetIncisionController();
+    }
+
+    [TestCaseSource(nameof(DivideCases))]
+    public async Task GetNumberOfPieces_ShouldReturnCorrectResult_WhenProvidedWithQuery(int amountOfPieces, string query)
+    {
+        var result = await _controller
+            .GetNumberOfPieces(query) as OkObjectResult;
+        var statusCode = result.StatusCode;
+
+        Assert.That(statusCode.Equals((int)HttpStatusCode.OK));
+        Assert.That(result.Value!.Equals(amountOfPieces));
+    }
+
+    [Test]
+    public async Task GetNumberOfPieces_ShouldReturnNoContentResult_WhenProvidedWithNoQuery()
+    {
+        var result = await _controller.GetNumberOfPieces(null) as NoContentResult;
+        var statusCode = result.StatusCode;
+
+        Assert.That(statusCode.Equals((int)HttpStatusCode.NoContent));
+    }
+}
